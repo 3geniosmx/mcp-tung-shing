b19e6da2f323e26019c068cfbf4b0f51b411a368
3geniosmx <81919263+3geniosmx@users.noreply.github.com>
2025-04-25 02:49:18 -0600

    morena2

--- rslib.config.js
+++ rslib.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from '@rslib/core';
 import { version } from "./package.json";
 
+
+
 export default defineConfig({
-  lib: [
-    {
-      format: 'esm',
-      syntax: 'es2021',
-      dts: false,  // Cambia a false para evitar problemas de tipos
-    },
-    {
-      format: 'cjs',
-      syntax: 'es2021',
-    },
+    lib: [
+      {
+        format: 'esm',
+        syntax: 'es2021',
+        dts: false,
+        external: ['@modelcontextprotocol/sdk', 'dayjs', 'express'] // Agregar dependencias externas
+      },
+      {
+        format: 'cjs',
+        syntax: 'es2021',
+        external: ['@modelcontextprotocol/sdk', 'dayjs', 'express'] // Agregar dependencias externas
+      },
+
   ],
   source: {
     define: {
@@ -19,3 +24,5 @@ export default defineConfig({
     }
   }
 });
+
+
